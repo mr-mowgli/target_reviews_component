26b546591a21fa09f77cfab74b1f32d3e0062b81
Jason Strunk <jasondstrunk@gmail.com>
2020-09-24 16:54:01 -0700

    Minor changes to server, rendering static HTML.

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/../Public'));
 
 app.get('/', (req, res) => {
-  res.send('SOME DATA')
+  res.send()
 })
 
 app.listen(port, () => {
