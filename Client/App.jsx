58304548599b4529f6bd6d6ad87eedce6f1a6a34
Jason Strunk <jasondstrunk@gmail.com>
2020-09-30 16:11:44 -0700

    Rendering Data properly

--- Client/App.jsx
+++ Client/App.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import List from './Components/List.jsx';
+
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      allData: ['some data here']
+    };
+  }
 
-    }
+  componentDidMount() {
+    fetch('http://localhost:3001/api/reviews')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          allData: data
+        })
+
+      })
   }
 
   render() {
     return (
-      <div>THIS IS MY APP</div>
+      <List allData={this.state.allData}/>
     );
   }
 }
