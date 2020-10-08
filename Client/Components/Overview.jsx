6d0688ecfb047397d6e750535bd9c9a060b4c3e5
Jason Strunk <jasondstrunk@gmail.com>
2020-10-08 16:02:34 -0700

    Implemented styled Stars component in Overview

--- Client/Components/Overview.jsx
+++ Client/Components/Overview.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from '../Styling/Styles.jsx';
+import { Button, Stars } from '../Styling/Styles.jsx';
 
 const Overview = (props) => {
 
@@ -7,18 +7,20 @@ const Overview = (props) => {
     <>
     <div className="overview">
         <h3>Guest Rating's and Reviews</h3>
-        <div className="overview item">BAR GRAPH</div>
-        <div className="overview item">5.0
+        <div className="overview-item">BAR GRAPH</div>
+        <div className="overview-stars"><h1>4.0</h1>
 
+        <Stars/>
+        <div>20 star ratings</div>
         </div>
         <div className="overview item">RECOMMEND</div>
 
         <div className="break"></div>
 
-        <div className="overview item">VALUE</div>
-        <div className="overview item">STYLE</div>
-        <div className="overview item">SIZING</div>
-        <div className="overview item">COMFORT</div>
+        <div className="overview-item">VALUE</div>
+        <div className="overview-item">STYLE</div>
+        <div className="overview-item">SIZING</div>
+        <div className="overview-item">COMFORT</div>
         <Button>Write a review</Button>
 
     </div>
--- Client/Styling/Styles.jsx
+++ Client/Styling/Styles.jsx
@@ -1,23 +1,32 @@
 import styled, { css } from 'styled-components';
 
 export const Button = styled.button`
-width: 100%;
 position: center;
+width: 100%;
 height: 30px;
 font-size: 12px;
-margin-bottom: 8px;
 padding: 0 4px;
-cursor: pointer;
 color: rgb(255, 255, 255);
 background-color: rgb(204, 0, 0);
 border-color: rgb(204, 0, 0);
 border-width: 1px;
 border-style: solid;
 border-radius: 4px;
-margin-left: 645px;
-margin-right: 645px;
+margin: 0px 475px 8px 475px;
+cursor: pointer;
 :hover {
 background-color: rgb(135, 0, 0);
 border-color: rgb(135, 0, 0);
 }
 `;
+
+export const Stars = styled.span`
+overflow: hidden;
+letter-spacing: 1px;
+margin: -20px 0px;
+color: rgb(255, 214, 2);
+&:before {
+  content: "\\2605\\2605\\2605\\2605";
+  -webkit-text-stroke: 0.75px rgb(204, 136, 14);
+}
+ `;
