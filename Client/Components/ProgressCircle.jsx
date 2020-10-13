7e974fe8699d27bd356073d92b124ea05a92e502
Jason Strunk <jasondstrunk@gmail.com>
2020-10-12 19:51:44 -0700

    Implemented Comfort Circle

--- Client/Components/ProgressCircle.jsx
+++ Client/Components/ProgressCircle.jsx
@@ -7,6 +7,7 @@ const ProgressCircle = (props) => {
     strokeWidth,
     circleOneStroke,
     circleTwoStroke,
+    complete
 } = props;
 
 const center = props.size / 2;
@@ -17,35 +18,28 @@ const styles= {
   color: 'red'
 };
 
+  if (props.complete === 5) {
+    return (
+      <>
+        <svg className="svg-progress" width={size} height={size}>
+          <circle
+            className="svg-circle"
+            stroke={circleTwoStroke}
+            cx={center}
+            cy={center}
+            r={radius}
+            strokeWidth={strokeWidth}
+            strokeDasharray={circumference}
+            strokeDashoffset={0}
+          />
+          <text id="svg-circle-text" x={25}  y={30} style={styles}>
+            100
+          </text>
 
-  return (
-    <>
-      <svg className="svg-progress" width={size} height={size}>
-        {/* <circle
-          className="svg-circle-bg"
-          stroke={circleOneStroke}
-          cx={center}
-          cy={center}
-          r={radius}
-          strokeWidth={strokeWidth}
-        /> */}
-        <circle
-          className="svg-circle"
-          stroke={circleTwoStroke}
-          cx={center}
-          cy={center}
-          r={radius}
-          strokeWidth={strokeWidth}
-          strokeDasharray={circumference}
-        />
-        <text id="svg-circle-text" x={25}  y={30} style={styles}>
-          100
-        </text>
-
-      </svg>
-    </>
-  )
-
+        </svg>
+      </>
+    )
+  }
 }
 
 export default ProgressCircle;
